970deba26b40a7b61d99c2dfbdf95ed5013da173
marcos costa <marcoscosta@MacBook-Pro-de-marcos.local>
2023-07-30 20:02:25 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-function atualizarTempo(){
-    var display = document.querySelector('.display');
-    var agora = new Date();
-    var horario = corrigirHorario(agora.getHours()) + ':'+ corrigirHorario(agora.getMinutes()) + ':'+ corrigirHorario(agora.getSeconds());
+const displayClock = document.querySelector('.digital-clock');
+const displayDate = document.querySelector('.display-date');
 
-    display.textContent = horario;
+const formatUnit = unit => `0${unit}`.slice(-2);
+
+const clockHTML = (hours, minutes, seconds) => `
+    <span>${formatUnit(hours)}</span> :
+    <span>${formatUnit(minutes)}</span> :
+    <span>${formatUnit(seconds)}</span>
+`;
+
+const dateHTML = (day, month, year) => `
+    <span>${formatUnit(day)}</span> /
+    <span>${formatUnit(month)}</span> /
+    <span>${formatUnit(year)}</span>
+`;
+
+const clockTime = () => {
+    const date = new Date();
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    displayClock.innerHTML = clockHTML(hours, minutes, seconds);
+
+    displayDate.innerHTML = dateHTML(day, month, year);
 
 }
-function corrigirHorario(numero){
-    if (numero < 10){
-        numero = '0'+ numero;
-    }
-    return numero;
-}
-atualizarTempo(); 
-setInterval(atualizarTempo, 1000);
-console.log(horario);
+
+clockTime();
+setInterval(clockTime, 1000);
